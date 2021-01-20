c56fdb68de25df5841400e952b328f9e1b19535e
fdamico <fabio.damico@codurance.com>
2021-01-20 09:48:54 +0100

    :recycle: Refactoring test code duplication

--- src/test/java/com/codurance/traineeship/katas/FibonacciShould.java
+++ src/test/java/com/codurance/traineeship/katas/FibonacciShould.java
@@ -3,25 +3,30 @@ package com.codurance.traineeship.katas;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FibonacciShould {
 
+  private Fibonacci fibonacci;
+
+  @BeforeEach
+  void setUp() {
+    fibonacci = new Fibonacci();
+  }
+
   @Test
   void give_a_zero_value_for_the_first_position() {
-    Fibonacci fibonacci = new Fibonacci();
     assertThat(fibonacci.at(0), is(0));
   }
 
   @Test
   void give_a_one_value_for_the_second_position() {
-    Fibonacci fibonacci = new Fibonacci();
     assertThat(fibonacci.at(1), is(1));
   }
 
   @Test
   void give_a_one_value_for_the_third_position() {
-    Fibonacci fibonacci = new Fibonacci();
     assertThat(fibonacci.at(2), is(1));
   }
 }
